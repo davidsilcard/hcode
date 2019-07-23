e267428db224f52f16659235169e7d09fd94952a
davidsilcard <davidsilcard@gmail.com>
2019-07-23 09:34:26 -0300

    finalizamos classe usuarios

--- DAO/index.php
+++ DAO/index.php
@@ -1,6 +1,6 @@
 <?php
 
     require_once("config.php");
-    $sql = new Sql();
-    $usuarios = $sql->select("select * from tb_usuarios");
-    echo json_encode($usuarios);
+    $usuario = new Usuario();
+    $usuario->loadById(3);
+    echo $usuario;
